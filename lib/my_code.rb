3bbdd229f8c1f8822dd8e587722c0663fdd1571d
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-06 02:33:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,4 +7,19 @@ def map(array)
     n += 1
   end
   negative_array
+end
+
+def reduce(array, start_value=nil) #start value optional
+  if start_value
+    total = start_value
+    n = 0
+  else
+    total = array[0]
+    n = 1
+  end
+  while n < array.length do
+    total = yield(total, array[n])
+    n += 1
+  end
+  total
 end 
